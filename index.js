23bf1ae2d563aa7be0168b350afa54ef57f8d4b3
Kingpin2557 <lode.snaet@student.howest.be>
2025-11-25 14:11:23 +0100

    Add support for dynamically managing sensors with POST endpoint

--- index.js
+++ index.js
@@ -4,26 +4,55 @@ const PORT = 8080;
 
 app.use(express.json());
 
+const sensors = [
+    {
+        id: 1,
+        data:[]
+    },
+    {
+        id: 2,
+        data:[]
+    },
+    {
+        id: 3,
+        data:[]
+    },
+    {
+        id: 4,
+        data:[]
+    }
+]
+
 app.get('/sensors', (req, res) => {
     res.status(200).send({
-        sensors: [ 1, 2, 3]
+        sensors: sensors
     });
 });
 
-app.post('/sensors/:id', (req, res) => {
-    const { id } = req.params;
-    const { logo } = req.body;
+app.post('/sensor', (req, res) => {
+    const newSensor = req.body;
 
-    if (!logo) {
-        return res.status(418).send({
-            error: 'Logo is required'
+    if (!newSensor || !newSensor.id) {
+        return res.status(400).send({
+            error: 'A sensor object with an ID is required in the body.'
         });
     }
 
-    res.send({
-        tshirt: `👕 with your logo: ${logo}, with ID of ${id}`
-    })
-});
+    const idExists = sensors.some(sensor => sensor.id === newSensor.id);
+
+    if (idExists) {
+        return res.status(400).send({
+            error: `Sensor ID ${newSensor.id} already exists.`
+        });
+    }
+
+    sensors.push(newSensor);
+
+    res.status(201).send({
+        message: `Sensor ID ${newSensor.id} added successfully.`,
+        data: newSensor
+    });
+})
 
 app.listen(
     PORT,
